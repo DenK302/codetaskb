2c1eb84cdbdfbb2064741ce61952953343d7378b
DenK302 <kdenis219@gmail.com>
2021-05-31 14:35:31 +0300

    Commit

--- codetaskb.py
+++ codetaskb.py
@@ -1,10 +1,13 @@
-from collections import Counter
-
-
 def first_repeat(sym_list):
-    for symbol, repeat_value in Counter(sym_list).items():
-        if repeat_value > 1:
-            return symbol
+    elements = set(sym_list)
+    list_index = [] #список для элементов и их индексов
+    diff_index = [] #список
+    for element in elements:
+        list_index.append([[index for index, symbol in enumerate(sym_list) if symbol == element], element])
+    for index, symbol in list_index:
+        if len(index) > 1:
+            diff_index.append([index[1] - index[0], str(symbol)])
+    return min(diff_index)[1]
 
 
-print(first_repeat(['a', 'b', 'c', 'c', 'a']))
+print(first_repeat(['a', 'c', 'b', 'b', 'c', 'a']))
